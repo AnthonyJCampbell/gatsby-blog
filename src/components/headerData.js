cb23ad65cb375053ba691e46478af9e6648e3ae3
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-28 21:04:46 +0100

    Initial commit

--- src/components/headerData.js
+++ src/components/headerData.js
@@ -4,9 +4,9 @@ import { FaGithub } from 'react-icons/fa';
 
 export const headerData = {
   menuItems: [
-    { title: 'BLOG', icon: <BM style={{ marginRight: 10, fontSize: 18 }} />, link: '/blog' },
+    { title: 'BLOG', icon: <BM style={{ marginRight: 10, fontSize: 18 }} />, link: 'https://anthonyjcampbell.com', external: true },
     {
-      title: 'GITHUB', icon: <FaGithub style={{ marginRight: 10 }} />, link: 'https://github.com/mattshardman', external: true,
+      title: 'GITHUB', icon: <FaGithub style={{ marginRight: 10 }} />, link: 'https://github.com/anthonyjcampbell', external: true,
     },
     { title: 'CONTACT', color: '#f442e5', link: '/contact' },
   ],
