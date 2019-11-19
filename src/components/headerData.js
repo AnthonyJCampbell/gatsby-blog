29bc9c769a114b21b92c57b220bb66f739a6a9e2
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-11-19 15:39:01 +0100

    Fixing GitHub link

--- src/components/headerData.js
+++ src/components/headerData.js
@@ -4,7 +4,9 @@ import { FaGithub } from 'react-icons/fa';
 
 export const headerData = {
   menuItems: [
-    { title: 'BLOG', icon: <BM style={{ marginRight: 10, fontSize: 18 }} />, link: 'https://anthonyjcampbell.com', external: true },
+    {
+      title: 'BLOG', icon: <BM style={{ marginRight: 10, fontSize: 18 }} />, link: 'https://anthonyjcampbell.com', external: true,
+    },
     {
       title: 'GITHUB', icon: <FaGithub style={{ marginRight: 10 }} />, link: 'https://github.com/anthonyjcampbell', external: true,
     },
